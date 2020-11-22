59e76c6e1dd1d9f62ea98a6594062bb41c9b12f3
Michael C. Iyke <ymichaelc@gmail.com>
2020-11-22 17:53:49 +0100

    Quick submit

---
+++ api/controllers/access/logout.js
@@ -0,0 +1,28 @@
+module.exports = {
+
+
+  friendlyName: 'Logout',
+
+
+  description: 'Logout access.',
+
+
+  inputs: {
+
+  },
+
+
+  exits: {
+
+  },
+
+
+  fn: async function (inputs) {
+
+    // All done.
+    return;
+
+  }
+
+
+};
